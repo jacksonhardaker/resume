6b582f36abb0f1481a3ecc203966a38dc92cb339
Jackson <jackson.hardaker@gmail.com>
2018-07-19 10:14:03 -0400

    Add dumb email and phone components

---
+++ src/components/EmailAddress.vue
@@ -0,0 +1,13 @@
+<template>
+  <a :href="`mailto:${$attrs.email}`">{{ $attrs.email }}</a>
+</template>
+
+<script>
+export default {
+  name: 'EmailAddress'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/PhoneNumber.vue
@@ -0,0 +1,13 @@
+<template>
+  <a :href="`tel:${$attrs.phone}`">{{ $attrs.phone }}</a>
+</template>
+
+<script>
+export default {
+  name: 'PhoneNumber'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
